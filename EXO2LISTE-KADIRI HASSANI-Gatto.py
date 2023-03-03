0f823ee1cc0d5df0a26e0fc2e19af9184cf9e704
salimkad <salimkadirih@gmail.com>
2023-03-03 23:20:54 +0100

    Ajout de fontion sous graphe partiel A moitié fonctionnelle

--- EXO2LISTE-KADIRI HASSANI-Gatto.py
+++ EXO2LISTE-KADIRI HASSANI-Gatto.py
@@ -22,6 +22,7 @@ def add_sommetL(G, i) : #ajoute un sommet à la liste de listes
     #sinon, on vérifie que le sommet n'existe pas déjà
     elif i["id"] not in temp : #si le sommet n'existe pas, on l'ajoute à la liste de listes
         j = copy.deepcopy(i)
+        #j["id"] = len(G) 
         G.append(j)
         
         
@@ -166,7 +167,7 @@ def est_partiel(G, G2):
     for i in range(len(G)):
         if G[i]["nom"] not in temp :
             return False
-
+    #print("temp de est_partiel = ", temp)
     if inclus_aretesL(G, G2):
             return True
     return False
@@ -180,41 +181,87 @@ def est_sous_graphe(G, G2):  #à revoir
         return False
     temp = []
     for i in range(len(G)):
-        if G2[i]["nom"] in G[i]["nom"]:
-            temp.append(G2[i]) #temp contient les noms des éléments en commun
-            print("temp = ",temp)
+        for j in range(len(G2)):
+            if G2[j]["nom"] in G[i]["nom"]:
+                temp.append(G2[i]) #temp contient les noms des éléments en commun
+                #print("temp = ",temp)
     #là mon temp contient tous les sommets, mais contient trop d'arêtes, je dois retirer celles des sommets qui n'existent plus
-    for i in range(len(temp)):
-        for j in temp[i]["aretes"]:
-            if j not in temp:
+    cpt:int = 0
+    for i in range(len(temp)): #i parcours temp
+        for j in temp[i]["aretes"]: #j parcours aretes de la case i
+            cpt = 0
+            for k in range (len(temp)): #k parcours temp
+                if temp[k]["id"] != j:
+                    cpt = cpt + 1
+            if cpt == len(temp):
                 temp[i]["aretes"].remove(j)
-    print("temp = ",temp)
-    if temp == G :
-        return True
+    #print("temp = ",temp)
+
+    for i in range(len(temp)):
+        for j in range(len (G)):
+            if temp[i]["nom"] == G[j]["nom"]:
+                if temp[i]["aretes"] != G[j]["aretes"]:
+                    return False
     
-    return False
+    return True
+
+def est_sous_graphe_partiel(G,G2): #à l'aire de fonctionner, à revoir
+#renvoie 1 si G est un sous-graphe partiel de G2, 0 sinon.
+
+    #On va définir tous les sous graphes de G2, et on test si c'est un graphe partiel :
+    #On va donc parcourir tous les sommets de G2, et on va les retirer un par un, et on va tester si le graphe obtenu est un graphe partiel de G2
+    #Si c'est le cas, on renvoie 1, sinon on renvoie 0
+    sousgraphe = False
+
+    if len(G) >= len(G2):
+        return False
+    
+    for i in range(len(G2)): #i = case de G2
+        temp = [] #sous-graphe de G2
+        for j in range(len(G2)): #j = case de G2
+            if j != i:
+                temp.append(G2[j]) #crée len(G2)-1 cases
+                if i >= len(G2) :
+                    for l in temp[j]["aretes"]:
+                        if l == i:
+                            temp[j]["aretes"].remove(i)
+        for k in range(len(temp)):
+            temp[k]["id"]= k
+        print("temp = ",temp)
+        if est_partiel(G, temp):
+            sousgraphe = True
+
+    return sousgraphe
+
+    if not est_partiel(G, G2):
+        return False
+    if not est_sous_graphe(G, G2):
+        return False
+    return True
+
+
 # TESTS
 #TEST LISTES
 
-A ={"id" : 0,"nom" : "A", "aretes" : []}
-B ={"id" : 1,"nom" : "B", "aretes" : []}
-C ={"id" : 2,"nom" : "C", "aretes" : []}
-D ={"id" : 3,"nom" : "D", "aretes" : []}
+S1 ={"id" : 0,"nom" : "A", "aretes" : []}
+S2 ={"id" : 1,"nom" : "B", "aretes" : []}
+S3 ={"id" : 2,"nom" : "C", "aretes" : []}
+S4 ={"id" : 3,"nom" : "D", "aretes" : []}
 
 
 grapheL = graphe_videL()
 
-add_sommetL(grapheL, A)
-add_sommetL(grapheL, B)
-add_sommetL(grapheL, C)
-add_sommetL(grapheL, D) 
+add_sommetL(grapheL, S1)
+add_sommetL(grapheL, S2)
+add_sommetL(grapheL, S3)
+add_sommetL(grapheL, S4) 
 
 
 grapheL2 = graphe_videL()
-add_sommetL(grapheL2, A)
-add_sommetL(grapheL2, B)
-add_sommetL(grapheL2, C)
-add_sommetL(grapheL2, D) 
+add_sommetL(grapheL2, S1)
+add_sommetL(grapheL2, S2)
+add_sommetL(grapheL2, S3)
+add_sommetL(grapheL2, S4) 
 
 
 print("graphe L :\n", grapheL)
@@ -224,37 +271,68 @@ print("graphe L2 :\n", grapheL2)
 
 print("Sommets de L inclus dans L2 ? ", inclus_sommetL(grapheL, grapheL2, 1))
 
-addL(grapheL, A, B)
-addL(grapheL, C, A)
+addL(grapheL, S1, S2)
+addL(grapheL, S3, S1)
 #addL(grapheL, C, B)
 #addL(grapheL, B, D)
 
 
-addL(grapheL2, A, B)
-addL(grapheL2, C, A)
-addL(grapheL2, C, B)
-addL(grapheL2, B, D)
-addL(grapheL2, C, D)
+addL(grapheL2, S1, S2)
+#addL(grapheL2, S3, S2)
+addL(grapheL2, S3, S1)
+addL(grapheL2, S1, S4)
+addL(grapheL2, S3, S4)
 
 
 print("graphe L :\n", grapheL)
 print("graphe L2 :\n", grapheL2)
-
+'''
 print("Aretes de L inclus dans L2 ? ", inclus_aretesL(grapheL, grapheL2))
 
 print("L est un graphe partiel de L2 ? ", est_partiel(grapheL, grapheL2))
 
 
 grapheL3 = graphe_videL()
-add_sommetL(grapheL3, A)
-add_sommetL(grapheL3, B)
-add_sommetL(grapheL3, C)
-addL(grapheL3,A,B)
-addL(grapheL3,C,A)
-addL(grapheL3,C,B)
+S5 ={"id" : 1,"nom" : "C", "aretes" : []}
+S6 ={"id" : 2,"nom" : "B", "aretes" : []}
+add_sommetL(grapheL3, S1)
+add_sommetL(grapheL3, S5)
+add_sommetL(grapheL3, S6)
+addL(grapheL3,S1,S5)
+addL(grapheL3,S6,S1)
+#addL(grapheL3,S5,S6)
 
 print("graphe L3 :\n", grapheL3)
 
 print("L3 est un sous-graphe de L2 ? ", est_sous_graphe(grapheL3, grapheL2))
+'''
 
+
+grapheL4 = graphe_videL()
+S7 ={"id" : 2,"nom" : "D", "aretes" : []}
+
+add_sommetL(grapheL4, S1)
+add_sommetL(grapheL4, S2)
+add_sommetL(grapheL4, S7)
+
+addL(grapheL4,S1,S2)
+#addL(grapheL4,S1,S7)
+
+print("graphe L4 :\n", grapheL4)
+
+grapheL5 = graphe_videL()
+add_sommetL(grapheL5, S1)
+add_sommetL(grapheL5, S2)
+add_sommetL(grapheL5, S3)
+add_sommetL(grapheL5, S4)
+
+addL(grapheL5,S1,S2)
+addL(grapheL5,S1,S4)
+addL(grapheL5,S2,S3)
+addL(grapheL5,S3,S4)
+addL(grapheL5,S2,S4)
+
+print("graphe L5 :\n", grapheL5)
+
+print("L4 est un sous-graphe partiel de L5 ? ", est_sous_graphe_partiel(grapheL4, grapheL5))
 
